8ba884ac1fdc5536e0cd83effcf43a53fb3891bc
Mlinda24 <Bsc-21-22@unima.ac.mw>
2026-05-12 18:49:11 -0800

    Fix DRF schema class - remove deprecated coreapi

--- FoodStore/FoodStore/urls.py
+++ FoodStore/FoodStore/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     # Swagger UI
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/notifications/', include('notifications.urls')),
+    #path('api/notifications/', include('notifications.urls')),
 
 ]
 
