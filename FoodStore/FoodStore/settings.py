c3b444db4161037cfade67740dc8287479dd1a24
MELISSAHZULU <bsc-04-22@unima.ac.mw>
2026-06-03 14:19:46 +0200

    fix: use PostgreSQL on Render via DATABASE_URL

--- FoodStore/FoodStore/settings.py
+++ FoodStore/FoodStore/settings.py
@@ -41,11 +41,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'FoodStore.urls'
 WSGI_APPLICATION = 'FoodStore.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        conn_max_age=600,
+        ssl_require=not DEBUG,
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.User'
