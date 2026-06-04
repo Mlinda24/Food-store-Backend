1a938e1523fc02ca3df1400b326ab9dc769ee5d3
MELISSAHZULU <bsc-04-22@unima.ac.mw>
2026-06-04 22:26:00 +0200

    urls

--- FoodStore/drivers/urls.py
+++ FoodStore/drivers/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import DriverViewSet, DeliveryOrderViewSet
 
 router = DefaultRouter()
-router.register(r'', DriverViewSet, basename='driver')
+router.register(r'drivers', DriverViewSet, basename='driver')
 router.register(r'delivery/orders', DeliveryOrderViewSet, basename='delivery-order')
 
 urlpatterns = [
